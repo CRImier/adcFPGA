53d89efe8b660c747ba7c0b46164f6e6ded5e4f2
root <root@DE1_SoC.usn.no>
2016-10-27 06:36:12 +0000

    readout in voltage

--- test_adc0.py
+++ test_adc0.py
@@ -2,6 +2,6 @@
 import devmem
 base=0xff200000
 adcChanel0=devmem.DevMem(base, 0x100, "/dev/mem", 0)
-adcValue=adcChanel0.read(0,1)[0]
-print("adc ch 0: %d " % adcValue)
+adcValue=(adcChanel0.read(0,1)[0] /993.0)
+print("adc ch 0: %.3f Volt " % adcValue)
  
